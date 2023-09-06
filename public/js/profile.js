ea1a679aa9c62788faf198cd3982bda73d2c5b42
Marina Del Conte <marinadelconte@yahoo.com>
2023-09-05 22:32:27 -0400

    added some stylistic changes

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,7 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#post-name').value.trim();
-  const description = document.querySelector('#post-desc').value.trim();
+  const description = document.querySelector('#post-dec').value.trim();
 
   if (name && description) {
     const response = await fetch(`/api/posts`, {
@@ -39,7 +39,7 @@ const delButtonHandler = async (event) => {
 
 document
   .querySelector('.new-post-form')
-  .addEventListener('submit', newFormHandler);
+  .addEventListener('save', newFormHandler);
 
 document
   .querySelector('.post-list')
